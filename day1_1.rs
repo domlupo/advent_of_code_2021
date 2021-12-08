f84e0892348b151cb87970276a94a3133f74f0cc
Dominic Lupo <dominiclupo@Dominics-MacBook-Pro.local>
2021-12-07 23:49:30 -0500

    add day 1 part 1

---
+++ day1_1.rs
@@ -0,0 +1,35 @@
+// https://github.com/rust-lang/rust/issues/62358
+// I tried to use Option<i32>.contains(&<i32>)
+// but realized this was an inprogress issue since 2019
+
+// TODO
+// remove s != "" check and use more rust orientated error handling
+// Better style and more robust
+
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("day1.txt")
+        .expect("Something went wrong reading the file");
+    let contents_split = contents.split("\n");
+   
+    let mut prior_num: Option<i32> = None;
+    let mut result = 0;
+
+    for s in contents_split {
+        if prior_num.is_some() && s != "" {
+            let current_num = s.parse::<i32>().unwrap();
+
+            if current_num > prior_num.unwrap() {
+                result += 1;
+            }
+
+        }
+
+        if s != "" {
+            prior_num = Some(s.parse::<i32>().unwrap());
+        }
+    }
+
+    println!("{}", result);
+}
