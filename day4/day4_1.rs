60bcb4c72f8876b21024a6729a30fe9b73b47a17
Dominic <dominiclupo5318@gmail.com>
2021-12-12 12:44:24 -0500

    add set_next_line method to UnparsedBingoBoard struct

--- day4/day4_1.rs
+++ day4/day4_1.rs
@@ -3,31 +3,36 @@
 use std::fs;
 
 const FILE_NAME: &str  = "day4.txt";
+const BINGO_BOARD_ROWS: i8 = 5;
+const BINGO_BOARD_COLS: i8 = 5;
 
 fn main() {
     let file_contents = fs::read_to_string(FILE_NAME)
         .expect("Something went wrong reading the file");
 
     let mut unparsed_called_nums: Option<String> = None;
-    let mut unparsed_bingo_boards: Vec<UnparsedBingoBoard>;
-    let mut unparsed_bingo_board;
+    let mut unparsed_bingo_boards: Vec<UnparsedBingoBoard> = Vec::new();
+    let mut unparsed_bingo_board = UnparsedBingoBoard { ..Default::default() };
 
     for line in file_contents.split("\n") {
         if unparsed_called_nums.is_none() {
             unparsed_called_nums = Some(line.to_string());
-        } else if line == "" {
-            unparsed_bingo_board = UnparsedBingoBoard { ..Default::default() };
-        } else {
-            unparsed_bingo_board = UnparsedBingoBoard { ..Default::default() };
+        } else if line != "" {
+            unparsed_bingo_board.set_next_line(line.to_string());
+
+            if unparsed_bingo_board.index == BINGO_BOARD_ROWS as usize{
+                unparsed_bingo_boards.push(unparsed_bingo_board.clone());
+            }
         }
     }
 
     println!("{}", unparsed_called_nums.unwrap());
 }
 
+#[derive(Clone)]
 struct UnparsedBingoBoard {
-    lines: [String; 5],
-    index: i8,
+    lines: [String; BINGO_BOARD_ROWS as usize],
+    index: usize,
 }
 
 impl Default for UnparsedBingoBoard {
@@ -39,13 +44,25 @@ impl Default for UnparsedBingoBoard {
     }
 }
 
+impl UnparsedBingoBoard {
+    fn set_next_line(&mut self, line: String) {
+        self.lines[self.index] = line;
+
+        if self.index + 1  == self.lines.len() {
+            self.index = 0;
+        } else {
+            self.index += 1;
+        }
+    }
+}
+
 struct BingoSquare {
     val: i64,
     marked: bool,
 }
 
 struct BingoBoard {
-    board: [BingoSquare; 25],
+    board: [BingoSquare; (BINGO_BOARD_ROWS * BINGO_BOARD_COLS) as usize ],
 }
 
 struct BingoBoardResult {
