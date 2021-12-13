06cf673dc3989136bffdacaae5885ffb27ba02a9
Dominic <dominiclupo5318@gmail.com>
2021-12-13 01:27:18 -0500

    Finish day 4 part 1

--- day4/day4_1.rs
+++ day4/day4_1.rs
@@ -1,10 +1,9 @@
-// Answer is TODO
-
+// Answer is 16674
 use std::fs;
 
 const FILE_NAME: &str  = "day4.txt";
-const BINGO_BOARD_ROWS: i8 = 5;
-const BINGO_BOARD_COLS: i8 = 5;
+const BINGO_BOARD_ROWS: u8 = 5;
+const BINGO_BOARD_COLS: u8 = 5;
 
 fn main() {
     let file_contents = fs::read_to_string(FILE_NAME)
@@ -32,6 +31,36 @@ fn main() {
     for unparsed_bingo_board in unparsed_bingo_boards {
         bingo_boards.push(BingoBoard::build(unparsed_bingo_board.get_parsed()));
     }
+
+    let called_nums: Vec<u8> = unparsed_called_nums
+        .unwrap()
+        .split(",")
+        .map(|str| str.parse::<u8>().unwrap())
+        .collect();
+
+    let bingo_board_results: Vec<BingoBoardResult> = bingo_boards
+        .into_iter()
+        .map(|mut bingo_board| bingo_board.get_result(&called_nums))
+        .collect();
+
+    let mut least_called_nums: Option<u64> = None;
+    let mut winning_board_score: Option<u64> = None;
+
+    for bingo_board_result in bingo_board_results {
+        if bingo_board_result.bingo {
+            if least_called_nums.is_none() {
+                least_called_nums = Some(bingo_board_result.count_called_nums);
+                winning_board_score = Some(bingo_board_result.sum_uncalled_nums *
+                                           bingo_board_result.last_num_called);
+            } else if bingo_board_result.count_called_nums < least_called_nums.unwrap() {
+                least_called_nums = Some(bingo_board_result.count_called_nums);
+                winning_board_score = Some(bingo_board_result.sum_uncalled_nums *
+                                           bingo_board_result.last_num_called);
+            }
+        }
+    }
+
+    println!("{}", winning_board_score.unwrap().to_string());
 }
 
 #[derive(Clone)]
@@ -73,7 +102,7 @@ impl UnparsedBingoBoard {
 
                 // .split does not support multiple delimiters, so handle " 2" -> "", "2"
                 if num != "" {
-                    parsed_bingo_board[i] = num.as_bytes()[0];
+                    parsed_bingo_board[i] = num.parse::<u8>().unwrap();
                     i += 1;
                 }
             }
@@ -84,15 +113,11 @@ impl UnparsedBingoBoard {
 }
 
 struct BingoSquare {
-    val: Option<u8>,
-    marked: bool,
+    num: Option<u8>,
+    called: bool,
 }
-
-impl Default for BingoSquare {
-    fn default() -> BingoSquare {
-        BingoSquare {
-            val: None,
-            marked: false,
+impl Default for BingoSquare { fn default() -> BingoSquare { BingoSquare { num: None,
+            called: false,
         }
     }
 }
@@ -102,23 +127,112 @@ struct BingoBoard {
 }
 
 impl BingoBoard {
-    fn build (bingo_vals: [u8; (BINGO_BOARD_ROWS * BINGO_BOARD_COLS) as usize]) -> BingoBoard {
+    fn build (bingo_nums: [u8; (BINGO_BOARD_ROWS * BINGO_BOARD_COLS) as usize]) -> BingoBoard {
         let mut squares:
             [BingoSquare; (BINGO_BOARD_ROWS * BINGO_BOARD_COLS) as usize] = Default::default();
         let mut i = 0;
 
-        for val in bingo_vals {
-            squares[i].val = Some(val);
+        for num in bingo_nums {
+            squares[i].num = Some(num);
             i += 1;
         }
 
         return BingoBoard { squares: squares }
+    }
 
+    fn get_result (&mut self, called_nums: &Vec<u8>) -> BingoBoardResult {
+        let mut last_num_called: Option<u64> = None;
+        let mut count_called_nums: u64 = 0;
+        let mut bingo = false;
+
+        for (i, num) in called_nums.iter().enumerate() {
+            last_num_called = Some(u64::from(num.clone()));
+            count_called_nums = i as u64;
+
+            self.mark_called_num(num);
+            if self.bingo() {
+                bingo = true;
+                break;
+            }
+        }
+
+        let mut sum_uncalled_nums = 0u64;
+
+        for bingo_square in &mut self.squares {
+            if bingo_square.called == false {
+                sum_uncalled_nums += bingo_square.num.unwrap() as u64;
+            }
+        }
+
+        return BingoBoardResult {
+            sum_uncalled_nums: sum_uncalled_nums,
+            last_num_called: last_num_called.unwrap(),
+            count_called_nums: count_called_nums,
+            bingo: bingo,
+        }
+    }
+
+    fn mark_called_num (&mut self, called_num: &u8) -> () {
+        for mut bingo_square in &mut self.squares {
+            if bingo_square.num.unwrap() == *called_num {
+                bingo_square.called = true;
+                return;
+            }
+        }
+    }
+
+    fn bingo (&self) -> bool {
+        if self.row_bingo() || self.col_bingo() {
+            return true;
+        }
+
+        return false;
+    }
+
+    fn row_bingo (&self) -> bool {
+        for i in 0..BINGO_BOARD_ROWS {
+            let mut called_count = 0;
+
+            for j in 0..BINGO_BOARD_COLS {
+                if self.squares[((i * BINGO_BOARD_COLS) + j) as usize].called {
+                    called_count += 1;
+                } else {
+                    break;
+                }
+            }
+
+            if called_count == BINGO_BOARD_COLS {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn col_bingo (&self) -> bool {
+        for i in 0..BINGO_BOARD_COLS {
+            let mut called_count = 0;
+
+            for j in 0..BINGO_BOARD_ROWS {
+                if self.squares[((j * BINGO_BOARD_COLS) + i) as usize].called {
+                    called_count += 1;
+                } else {
+                    break;
+                }
+            }
+
+            if called_count == BINGO_BOARD_ROWS {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
 struct BingoBoardResult {
-    sum_uncalled_nums: i64,
-    last_num_called: i64,
-    count_called_nums: i64,
+    sum_uncalled_nums: u64,
+    last_num_called: u64,
+    count_called_nums: u64,
+    bingo: bool,
 }
