0ef0e69b8913a95b34ad3ffb91a16575846ee3dd
Dominic <dominiclupo5318@gmail.com>
2021-12-15 10:59:11 -0500

    Add day 7 part 1

---
+++ day7/day7_1.rs
@@ -0,0 +1,36 @@
+// Answer is 351901
+use std::fs;
+
+const FILE_NAME: &str = "day7.txt";
+
+fn main() {
+    let mut file_contents = fs::read_to_string(FILE_NAME)
+        .expect("Something went wrong reading the file");
+
+    file_contents.pop();
+
+    let min = file_contents
+        .split(",")
+        .map(|x| x.parse::<u64>().unwrap())
+        .min().unwrap();
+
+    let max = file_contents
+        .split(",")
+        .map(|x| x.parse::<u64>().unwrap())
+        .max().unwrap();
+
+    let mut min_fuel_needed: Option<i64> = None;
+    for position in min..=max {
+        let mut fuel_total = 0;
+        
+        for crab_position in file_contents.split(",") {
+            fuel_total += (crab_position.parse::<i64>().unwrap() - position as i64).abs();
+        }
+
+        if min_fuel_needed.is_none() || fuel_total < min_fuel_needed.unwrap() {
+            min_fuel_needed = Some(fuel_total);
+        }
+    }
+
+    println!("{}", min_fuel_needed.unwrap().to_string());
+}
